e87e84369f70c7bee679f1734ff17c09d0e9d816
ermal96 <ermalvrapi18@gmail.com>
2019-10-07 07:30:26 +0200

    added signlework componet

--- src/App.js
+++ src/App.js
@@ -1,21 +1,30 @@
 import React from 'react';
-import {Header, Home, About, Works, Contact } from "./Components/index";
-import {BrowserRouter, Route, Switch} from 'react-router-dom';
+import {
+  Header,
+  Home,
+  About,
+  Works,
+  Contact,
+  SingleWork
+} from "./Components/index";
+
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 
 export default function App() {
   return (
-
-    <BrowserRouter>
+    <Router>
 
       <Header/>
+
       <Switch>
         <Route path="/" component={Home} exact/>
         <Route path="/about" component={About}/>
-        <Route path="/works" component={Works}/>
+        <Route path="/works" component={Works} exact/>
+        <Route path="/works/:slug" component={SingleWork}/>
         <Route path="/contact" component={Contact}/>
       </Switch>
-
-    </BrowserRouter>
+      
+    </Router>
 
   );
 }
