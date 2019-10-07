1b7985d2edd2eb6fff6c3c4973210bf37b4189f6
ermal96 <ermalvrapi18@gmail.com>
2019-10-07 07:31:36 +0200

    exprted as default singlework component

--- src/Components/index.js
+++ src/Components/index.js
@@ -3,3 +3,4 @@ export {default as  Contact }  from "./Contact/Contact";
 export {default as  Header }  from "./Header/Header";
 export {default as  Home } from "./Home/Home";
 export {default as  Works }  from "./Works/Works";
+export {default as  SingleWork }  from "./Works/SingleWork";
