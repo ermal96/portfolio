a5b47f60cd0dcd521e3d7a7e00a3cde628909ef7
ermal96 <ermalvrapi18@gmail.com>
2019-10-11 14:36:08 +0200

    added ga

--- src/index.js
+++ src/index.js
@@ -3,7 +3,12 @@ import ReactDOM from 'react-dom';
 import './App.css';
 import './Animate.css';
 import App from './App';
+import ReactGA from 'react-ga';
 import * as serviceWorker from './serviceWorker';
+
+ReactGA.initialize('UA-119596004-2');
+ReactGA.pageview(window.location.pathname + window.location.search);
+
 ReactDOM.render(
   <App/>, document.getElementById('root'));
 
